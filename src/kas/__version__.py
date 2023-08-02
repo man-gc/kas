8e66de928ac4dd158ad225da392b5383f2c5df29
Mathieu Anquetin <mathieu.anquetin@groupe-cahors.com>
2023-08-02 09:59:55 +0200

    Use dynamic version from SCM
    Signed-off-by: Mathieu Anquetin <mathieu.anquetin@groupe-cahors.com>

--- src/kas/__version__.py
+++ src/kas/__version__.py
@@ -9,7 +9,19 @@
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) Siemens AG, 2017-2020'
 
-__version__ = '4.0'
+# Try to define version
+try:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="../..", relative_to=__file__)
+except (ImportError, LookupError):
+    try:
+        from ._version import __version__  # noqa: F401
+    except ModuleNotFoundError:
+        raise RuntimeError(
+            "kas is not correctly installed. "
+            "Please install it with pip."
+        )
 
 # Please update docs/format-changelog.rst when changing the file version.
 __file_version__ = 14
